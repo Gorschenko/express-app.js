4ff4c034a1b4d58fd88e6c736360d4e444cee2f8
Alexey <aa.gorschenko@yandex.ru>
2022-09-04 16:50:19 +0300

    HTML coding was added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
 
@@ -14,16 +17,9 @@ app.set('view engine', 'hbs') // используем hbs
 app.set('views', 'views')
 
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.status(200)
-    res.render('index')
-    console.log('DIRNAME: ', __dirname)
-})
-
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 const PORT = process.env.PORT || 3000
 
