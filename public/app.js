601710ffc3a17283030e7ae570aaeff9f516cd8d
Alexey <aa.gorschenko@yandex.ru>
2022-09-07 14:11:32 +0300

    New page was added

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// Клиентский скрипт
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = new Intl.NumberFormat(
+        'ru-RU',
+        {
+            currency: 'rub',
+            style: 'currency'
+        }.format(node.textContent)
+    )
+})
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,25 @@
-const {Router} = require('express')
+const {Router, response} = require('express')
+const Course = require('../models/courses')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Course.getAll()
     res.render('courses', {
         title: 'Курсы',
         isCourses: true,
+        courses,
     })
+})
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id)
+    res.render(
+        'course',
+         {
+            course,
+            layout: 'empty',
+         }
+    )
 })
 
 module.exports = router
