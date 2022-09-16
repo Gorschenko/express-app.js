d39c494ee9b41f497ac280d07795fed77863fb70
Alexey <aa.gorschenko@yandex.ru>
2022-09-16 13:09:51 +0300

    Sessions were added. Defender of routes was added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session) // подключение сессий к базе данных
 const homeRoutes = require('./routes/home')
 const ordersRoutes = require('./routes/orders')
 const cardRoutes = require('./routes/card')
@@ -11,31 +13,33 @@ const addRoutes = require('./routes/add')
 const authRoutes = require('./routes/auth')
 const coursesRoutes = require('./routes/courses')
 const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
 
+const MONGODB_URI = 'mongodb+srv://Gorschenko:uf7tBtVud0ef30Gk@cluster0.ynpkt15.mongodb.net/shop'
 const app = express()
-
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
     handlebars: allowInsecurePrototypeAccess(Handlebars),
+})
+const store = new MongoStore({ // подключение сессий к базе данных
+    collection: 'sessions',
+    uri: MONGODB_URI,
 })
 
 app.engine('hbs', hbs.engine) // регестрируем hbs
 app.set('view engine', 'hbs') // используем hbs
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('6322f3a6d03fdd13d280b048')
-        req.user = user
-        next()
-    } catch(e) {
-        console.log(e)
-    }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ // подключение сессий
+    secret: 'some secret value',
+    resave: false,
+    saveUninitialized: false,
+    store,
+}))
+app.use(varMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/orders', ordersRoutes)
@@ -48,20 +52,7 @@ const PORT = process.env.PORT || 3000
 
 const start = async () => {
     try {
-        const url = 'mongodb+srv://Gorschenko:uf7tBtVud0ef30Gk@cluster0.ynpkt15.mongodb.net/shop'
-        await mongoose.connect(url, {useNewUrlParser: true})
-
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'test@yandex.ru',
-                name: 'Test',
-                cart: {
-                    items: []
-                }
-            })
-            await user.save()
-        }
+        await mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
+const User = require('../models/user')
 const {Router} = require('express')
 const router = Router()
+
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         isLogin: true,
+    })
+})
+
+router.get('/logout', async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login#login')
+    })
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findById('6322f3a6d03fdd13d280b048')
+    req.session.user = user
+    req.session.isAuthenticated = true
+    req.session.save(err => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
     })
 })
 
